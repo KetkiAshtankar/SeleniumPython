170bd788a38238700c365e19ecda27df592671f5
Ketki Ashtankar <55389728+KetkiAshtankar@users.noreply.github.com>
2024-07-26 01:19:12 +0530

    Update alert.py
    updating with waits and scroll

--- alert.py
+++ alert.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,12 +10,16 @@ driver = webdriver.Chrome()
 # Navigate to the URL
 driver.get("https://www.hyrtutorials.com/p/alertsdemo.html")
 
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//button[@id='alertBox']"))
-)
+# element = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.XPATH, "//button[@id='alertBox']"))
+# )
+time.sleep(20)
 # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='btn btn-danger']")))
-driver.find_element(By.XPATH,"//button[@id='alertBox']").click()
+alert = driver.find_element(By.XPATH,"//button[@id='alertBox']")
+driver.execute_script("arguments[0].scrollIntoView();", alert)
+alert.click()
 
 alert = driver.switch_to.alert
 print("Alert text:", alert.text)  # Print the alert text
+time.sleep(5)
 alert.accept()
